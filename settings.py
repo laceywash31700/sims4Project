620e2e7374603021b6d0cf64b4833644d68bf909
Lacey W <lacey.m.washington@gmail.com>
2023-11-15 17:47:34 -0600

    added files for sims 4 mod

---
+++ settings.py
@@ -0,0 +1,8 @@
+import os
+
+creator_name = 'Din0saursN5pace'
+mods_folder = os.path.expanduser(
+    os.path.join('~', 'Documents', 'Electronic Arts', 'The Sims 4', 'Mods')
+)
+
+game_folder = os.path.join('D:', 'The Sims 4')
